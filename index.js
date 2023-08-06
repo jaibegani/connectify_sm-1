6a533ae00a31427a100c5e043d50021117cc7b4c
jaibegani <109041722+jaibegani@users.noreply.github.com>
2023-08-06 21:43:04 +0530

    first commit

--- index.js
+++ index.js
@@ -78,6 +78,7 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
+mongoose.set('strictQuery', true);
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
